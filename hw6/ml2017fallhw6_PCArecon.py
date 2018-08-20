230fca61a0a4b0ebfb271c7f2f23fb492b17e6ee
Kunxian Huang <b89202027@gmail.com>
2018-08-20 19:56:35 +0800

    add reconstruction of PCA

---
+++ hw6/ml2017fallhw6_PCArecon.py
@@ -0,0 +1,89 @@
+#!/bin/env python3
+#*-* coding=utf-8 *-*            
+
+import os,sys
+import argparse
+import glob
+import numpy as np
+from skimage import io, transform
+from utils.util import resizepic, minusavg
+
+def draw2face(img_origin, img_recon, plt, savefn=''):
+    img_recon -= np.min(img_recon) 
+    img_recon /= np.max(img_recon) 
+    img_recon = (img_recon*255).astype(np.uint8)
+
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8,4))
+
+    ca1 = ax1.imshow(img_origin, interpolation='nearest')
+    frame = plt.gcf()
+    title = 'Original pic:'
+    ax1.set_title(title)
+
+    ca2 = ax2.imshow(img_recon, interpolation='nearest')
+    cbar2 = fig.colorbar(ca2,ax=ax2)
+    plt.tight_layout()
+    frame = plt.gcf()
+    ax2.set_title('PCA reconstructed pic:')
+    
+    # given filename                  
+    if len(savefn)>0:
+        plt.savefig(savefn)
+    else:
+        #plt.imshow(saveimg)
+        plt.show()
+
+    return
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='ml2017fallhw6_PCArecon.py')
+    parser.add_argument('--datadir',type=str,dest="datadir",default='data/Aberdeen/')
+    parser.add_argument('--picnumber',type=int,default=0)
+    parser.add_argument('--newsize',type=int,default=60)
+    parser.add_argument('--klargest',type=int,default=4)
+    parser.add_argument('--loadU',type=str,default='pcapicU.npy')
+    args = parser.parse_args()
+
+    picfname = "%s/%d.jpg" %(args.datadir, args.picnumber)
+
+    #checking the file existing or not
+    if (os.path.exists(picfname)==False):
+        print("The file {} is not found....".format(picfname))
+        os.exit(0)
+        return
+
+    
+    img_origin = io.imread(picfname)
+    img_small = resizepic(img_origin, args)
+    img_minus, img_minusmap = minusavg(img_small)
+    img_flat = img_minus.flatten()
+    
+    print("Shape of flattened picture is {}".format(img_flat.shape))
+    
+    # load U matrix
+    U = np.load(args.loadU)
+    # checking dimension length of U with image dimension length
+    if (U.shape[0]!=img_flat.shape[0]):
+        print("Dimension length of U {} is not the same with the picture {}!".format(U.shape[0],img_flat.shape[0]))
+        return
+    # using k largest eigenface
+    k = args.klargest
+    Uk = U[:,:k]
+
+    dot_prods = np.dot(Uk.T, img_flat)
+    #print("shape of dot_prods is {}".format(dot_prods.shape))
+
+    img_flat_recon = np.dot(dot_prods.T, Uk.T)    
+    #print("shape of reconstructed flat is {}".format(img_flat_recon.shape))
+    img_recon = np.reshape(img_flat_recon,  (args.newsize,args.newsize,3)) + img_minusmap
+
+    resultfn = "result/PCApic_%d_k%d.png" %(args.picnumber,k)
+    import matplotlib.pyplot as plt
+    draw2face(img_small, img_recon, plt, resultfn)
+    
+    
+    return
+
+if __name__=='__main__':
+    main()
--- hw6/utils/util.py
+++ hw6/utils/util.py
@@ -6,6 +6,8 @@ import numpy as np
 import tensorflow as tf
 import _pickle as pk
 import jieba
+import argparse
+from skimage import io, transform
 from collections import Counter
 from nltk.corpus import stopwords
 from glove import Corpus, Glove
@@ -36,81 +38,28 @@ def build_dict( words, vocab_size):
     print('Most common words (+UNK)', count[:5])
     return count, dictionary, reversed_dictionary
 
-class DataManager(object):
-    def __init__(self):
-        self.data = {}
-        self.stop = stopwords.words('chinese')
-        # Read data from data_path
-        #  name       : string, name of data, usually 'train' 'test' 'semi'
-        #  with_label : bool, read data with label or without label
-    def add_data(self,name, data_path, with_label=False):
-        print ('read data from %s...'%data_path)
-        X, Y = [], []
-        with open(data_path,'r') as f:
-            for line in f:
-                if with_label:
-                    lines = line.strip().split(' +++$+++ ')
-                    X.append(lines[1])
-                    Y.append(int(lines[0]))
-                else:
-                    X.append(line)
+#resize picture to smaller size                            
+def resizepic(large_img, args):
+    new_shape = (args.newsize, args.newsize, 3)
+    small_img = transform.resize(large_img, new_shape)
 
-        if with_label:
-            self.data[name] = [X,Y]
-        else:
-            self.data[name] = [X]
+    return small_img
 
-    # Build dictionary
-    #  vocab_size : maximum number of word in your dictionary
-    def build_dict(self, keys,vocab_size, jiebacut=True):
-        print ('build new vocabulary dictionary')
-        # collection of words
-        jieba.initialize()
-        jieba.set_dictionary('../big5dict/dict.txt.big')
-        words = []
-        for key in keys:
-            for sentence in self.data[key][0]:
-                #words.extend(jieta.cut(sentence, cut_all=False))
-                ws_insen = jieta.cut(sentence, cut_all=jiebacut)
-                ws_remove = [ws for ws in ws_insen
-                             if ws not in self.stop]
-                words.extend(ws_remove)
+#minus avg of each channel                              
+def minusavg(ori_img):
+    img_shape = ori_img.shape
+    img_chflat = np.reshape(ori_img, (img_shape[0]*img_shape[1], img_shape[2]))
+    chn_avg = np.mean(img_chflat, axis=1)
+    minus_maplist =[]
+    chs = ["r","g","b"]
+    for ich, ch in enumerate(chs):
+        chmap = chn_avg[ich]*np.ones((img_shape[0], img_shape[1]))
+        minus_maplist.append(chmap)
+    minus_map = np.stack((minus_maplist[0],minus_maplist[1],minus_maplist[2]),axis=2)
+    new_img = ori_img - minus_map
 
+    return new_img, minus_map
 
-        count = [['UNK', -1]]
-        count.extend(Counter(words).most_common(vocab_size - 1))
-        self.dictionary = dict()
-        for word, _ in count:
-            dictionary[word] = len(self.dictionary)
-            
-        #data = list()
-        unk_count = 0
-        
-        for word in words:
-            index = self.dictionary.get(word, 0)
-            if index == 0:  # dictionary['UNK']
-                unk_count += 1
-                #data.append(index)
-    
-        count[0][1] = unk_count
-        self.reversed_dictionary = dict(zip(self.dictionary.values(), self.dictionary.keys()))
-        #return  count, self.dictionary, self.reversed_dictionary
-        print('Most common words (+UNK)', count[:5])
-        return count
-
-
-    # Save dictionary and reversed dictionary to specified path
-    def save_dictionary(self, path):
-        print ('save tokenizer to %s'%path)
-        pk.dump((self.dictionary, self.reversed_dictionary), open(path, 'wb'))
-    
-
-    # Load dictionary and reversed dictionary from specified path
-    def load_dictionary(self,path):
-        print ('Load dictionary from %s'%path)
-        self.dictionary, self.reversed_dictionary = pk.load(open(path, 'rb'))
-        
-        return
     
 
     
